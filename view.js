53e12695686e16217a5949adb65ad2528265d2e8
VaporFoxLord <ldonald298@gmail.com>
2020-05-03 16:12:26 +0500

    OwnerViewPage update

--- view.js
+++ view.js
@@ -3,7 +3,7 @@
 var storage = firebase.storage();
 var storageRef = storage.ref();
 //urll is the url for image
-storageRef.child("1588312025234-zad.png").getDownloadURL().then(function(url) {
+storageRef.child("dog.jpg").getDownloadURL().then(function(url) {
   // Or inserted into an <img> element:
   var img = document.getElementById('myImgId');
   img.src = url;
