6a886fc8fc3b28bc40b8b3f303c9b5c62695bd08
VaporFoxLord <ldonald298@gmail.com>
2020-06-05 08:27:19 +0500

    Show next image to label

--- view.js
+++ view.js
@@ -5,43 +5,17 @@ var rootRef = firebase.database().ref().child('Images');
 console.log(rootRef);
 var myList = new Array();
 // myList.push("ds")
-storageRef.child("dog.jpg").getDownloadURL().then(function(url) {
-  // Or inserted into an <img> element:
-  var img = document.getElementById('myImgId');
-  img.src = url;
-}).catch(function(error) {
-  // Handle any errors
-});
 
-storageRef.child('dog.jpg').getDownloadURL().then(function(url) {
-    // `url` is the download URL for 'images/stars.jpg'
 
-    // This can be downloaded directly:
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = function(event) {
-      var blob = xhr.response;
-    };
-    xhr.open('GET', url);
-    xhr.send();
 
-    // Or inserted into an <img> element:
-    var img = document.getElementById('myimg');
-    img.src = url;
-    }).catch(function(error) {
-    // Handle any errors
-});
-
-rootRef.on('child_added', snap => {
-  var img_name = snap.child("url").val();
-});
 
 
 
 var count = 0;
 // var userId = firebase.auth().currentUser.uid;
 var userID = ""
-var fbRef = firebase.database().ref().child("Images");
+var fbRef = firebase.database().ref().child("users");
+var imageRef = firebase.database().ref().child("Images");
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -49,36 +23,97 @@ firebase.auth().onAuthStateChanged(function(user) {
         var user = firebase.auth().currentUser.uid;
         userID = user;
         // alert(user);    //you should have your user here!
-        fbRef.on("child_added", snap => {
-            var key=snap.key;
-            myList.push(key);
-            // alert('HEllo');
-            // alert(key);
-        });
+
     } else {
         console.log('No user is signed in.');
     }
 });
-function nextImg(){
-    count = count+1;
-    var lbl = ""
-    var ele = document.getElementsByName('label');
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        lbl = ele[i].value;
-        if (myList.length <= count){
-          storageRef.child(myList[count]).getDownloadURL().then(function(url) {
-          // Or inserted into an <img> element:
-            img.src = url;
-            firebase.database().ref('users/' + userId).set({
-                label: lbl
 
-            }).catch(function(error) {
-              // Handle any errors
-            });
-          })
-        }
-      }
+imageRef.on("child_added", snap => {
+    var key=snap.key;
+    // alert(myList.length);
+    var name = snap.child("url").val();
+    myList.push(name);
+    // alert(name);
+    // alert(key);
+});
+// alert(myList.length);
+
+
+// storageRef.child('dog.jpg').getDownloadURL().then(function(url) {
+//     // `url` is the download URL for 'images/stars.jpg'
+//
+//     // This can be downloaded directly:
+//     var xhr = new XMLHttpRequest();
+//     xhr.responseType = 'blob';
+//     xhr.onload = function(event) {
+//       var blob = xhr.response;
+//     };
+//     xhr.open('GET', url);
+//     xhr.send();
+//
+//     // Or inserted into an <img> element:
+//     var img = document.getElementById('myimg');
+//     img.src = url;
+//     }).catch(function(error) {
+//     // Handle any errors
+// });
+//
+// rootRef.on('child_added', snap => {
+//   var img_name = snap.child("url").val();
+// });
+
+storageRef.child('dog.jpg').getDownloadURL().then(function(url) {
+  // Or inserted into an <img> element:
+  var img = document.getElementById('myImgId');
+  img.src = url;
+}).catch(function(error) {
+  // Handle any errors
+});
+
+function nextImg(){
+
+    var lbl = ""
+    // alert(myList[0]);
+
+    var ele = document.getElementsByName('label');
+    if (count < myList.length){
+      storageRef.child(myList[count]).getDownloadURL().then(function(url) {
+        // Or inserted into an <img> element:
+        var img = document.getElementById('myImgId');
+        img.src = url;
+      }).catch(function(error) {
+        // Handle any errors
+      });
+    }else {
+      alert("congratulations you have labeled all the projects");
+    }
+
+    // for(i = 0; i < ele.length; i++) {
+    //     if(ele[i].checked)
+    //     lbl = ele[i].value;
+    //     if (myList.length <= count){
+    //       storageRef.child(myList[count]).getDownloadURL().then(function(url) {
+    //         // Or inserted into an <img> element:
+    //         var img = document.getElementById('myImgId');
+    //         img.src = url;
+    //       }).catch(function(error) {
+    //         // Handle any errors
+    //       });
+    //       // alert(myList[count]);
+    //       // storageRef.child(myList[count]).getDownloadURL().then(function(url) {
+    //       // // Or inserted into an <img> element:
+    //       //   img.src = url;
+    //       //   firebase.database().ref('users/' + userId).set({
+    //       //       label: lbl
+    //       //
+    //       //   }).catch(function(error) {
+    //       //     // Handle any errors
+    //       //   });
+    //       // })
+    //     }
+    //   }
+      count = count+1;
 }
 //
 for (var i = 0; i < myList.length; i ++ ){
